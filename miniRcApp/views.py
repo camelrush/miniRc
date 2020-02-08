abcc79cff58fb2f21c8107a90b455b102f406f26
Shinya Takada <shinya.pyonkici@gmail.com>
2020-02-08 14:41:45 +0900

    2nd_update

--- miniRcApp/views.py
+++ miniRcApp/views.py
@@ -17,6 +17,33 @@ def control(request):
         th.start()
 
     return HttpResponse("forward:" + str(apps.forward_val) + " turn:" + str(apps.turn_val))
+'''
+    PIN1 = 4
+    PIN2 = 17
+    pi = pigpio.pi()
+    try:
+        setup_PWM(PIN1 ,200, 100)
+        setup_PWM(PIN2 ,10, 100)
+        for i in range(3):
+            lighting(PIN1 ,100 ,10 ,0.05)
+            lighting(PIN2 ,100 ,10 ,0.05)
+    except KeyboardInterrupt:
+        pass
+
+def setup_PWM(PIN ,f ,r):
+    pi.set_PWM_frequecy(PIN ,f)
+    pi.set_PWM_range(PIN ,r)
+
+def lighting(PIN ,vmax ,d, t):
+    v = 0
+    while v>= 0:
+        pi.set_PWM_dutycycle(PIN ,v)
+        time.sleep(t)
+        if v >= vmax:
+            d = d * -1
+            v = v + d
+
+'''
 
 def index_template(request):
     return render(request, 'index.html')
