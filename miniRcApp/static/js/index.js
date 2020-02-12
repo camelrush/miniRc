c7675a0fca498b4044f2fb518d6af629d8a9b900
Shinya Takada <shinya.pyonkici@gmail.com>
2020-02-13 02:32:37 +0900

    3rd_commit

--- miniRcApp/static/js/index.js
+++ miniRcApp/static/js/index.js
@@ -3,26 +3,105 @@ $(document).ready(function() {
     $("#slider-bar-virtical").slider({
         orientation: 'vertical',
         animate: 'slow',
-        value: 5,
-        min: 0,
-        max: 10,
+        value: 0,
+        min: -5,
+        max: 5,
         step: 1,
         range: "min",
-        slide: function(event, ui) {
-            $("#slider-value-virtical").html(ui.value - (10 / 2));
+        change: function(event, ui) {
+            $("#slider-value-virtical").html(ui.value);
+            control_send();
         }
     });
 
     $("#slider-bar-horizon").slider({
         orientation: 'horizon',
         animate: 'slow',
-        value: 5,
-        min: 0,
-        max: 10,
+        value: 0,
+        min: -5,
+        max: 5,
         step: 1,
         range: "min",
-        slide: function(event, ui) {
-            $("#slider-value-horizon").html(ui.value - (10 / 2));
+        change: function(event, ui) {
+            $("#slider-value-horizon").html(ui.value);
+            control_send();
         }
     });
+
+    $("#slider-arrow-up").click(function() {
+        var val = $("#slider-bar-virtical").slider("value");
+        $("#slider-bar-virtical").slider("value", val + 1);
+    });
+
+    $("#slider-arrow-down").click(function() {
+        var val = $("#slider-bar-virtical").slider("value");
+        $("#slider-bar-virtical").slider("value", val - 1);
+    });
+
+    $("#slider-arrow-left").click(function() {
+        var val = $("#slider-bar-horizon").slider("value");
+        $("#slider-bar-horizon").slider("value", val - 1);
+    });
+
+    $("#slider-arrow-right").click(function() {
+        var val = $("#slider-bar-horizon").slider("value");
+        $("#slider-bar-horizon").slider("value", val + 1);
+    });
+
+    var control_send = function() {
+
+        var vval = $("#slider-bar-virtical").slider("value");
+        var hval = $("#slider-bar-horizon").slider("value");
+
+        var data = {
+            direction: "left",
+            virtical: vval,
+            horizon: hval
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "http://192.168.3.24:3000/control/",
+            data: data,
+            dataType: "html"
+        }).done(function(data, textStatus, jqXHR) {
+            alert(data);
+        });
+    };
+
+    // ==============================================================
+    // Ajax to django by POST functions has probrem.
+    // refer to https://docs.djangoproject.com/en/dev/ref/csrf/#ajax
+    // ==============================================================
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    var csrftoken = getCookie('csrftoken');
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
 });
--- miniRcApp/views.py
+++ miniRcApp/views.py
@@ -11,39 +11,13 @@ ON_CONTINUE_SECONDS = 3
 
 # Create your views here.
 def control(request):
-    control = direction.Direction.get_control(request.GET.get("direction"))
+    control = direction.Direction.get_control(request.POST.get("direction"))
+    print(request.POST.get("horizon"))
+    print(request.POST.get("virtical"))
     if not control is None:
         th = threading.Thread(target=action_exec(control))
         th.start()
-
     return HttpResponse("forward:" + str(apps.forward_val) + " turn:" + str(apps.turn_val))
-'''
-    PIN1 = 4
-    PIN2 = 17
-    pi = pigpio.pi()
-    try:
-        setup_PWM(PIN1 ,200, 100)
-        setup_PWM(PIN2 ,10, 100)
-        for i in range(3):
-            lighting(PIN1 ,100 ,10 ,0.05)
-            lighting(PIN2 ,100 ,10 ,0.05)
-    except KeyboardInterrupt:
-        pass
-
-def setup_PWM(PIN ,f ,r):
-    pi.set_PWM_frequecy(PIN ,f)
-    pi.set_PWM_range(PIN ,r)
-
-def lighting(PIN ,vmax ,d, t):
-    v = 0
-    while v>= 0:
-        pi.set_PWM_dutycycle(PIN ,v)
-        time.sleep(t)
-        if v >= vmax:
-            d = d * -1
-            v = v + d
-
-'''
 
 def index_template(request):
     return render(request, 'index.html')
